8c8741be2b407b70b5f20eeb437c7c320aba77c8
Alejandro Enríquez <alexalejandroem@gmail.com>
2016-10-14 22:39:23 -0600

    Se inició el desarrollo de el acceso a la base de datos

---
+++ APPrendo/platforms/browser/www/index.js
@@ -0,0 +1,30 @@
+//JQuery
+// $(elemento a buscar). accion (accion a ejecutar)
+//cuando el documento este cargado por completo que ejecute la funcion
+$(document).ready(function(){
+    //cuando el formulario con class login, realice la accion submit ejecute la funcion.
+    $('.LogIn').on('submit', function () {
+        //guardando el valor de el input con id email
+        var correo = $("#Email").val();
+        // guardo el valor del input con el id password
+        var contrasena =$("#Password").val();
+        //
+        //http://localhost/login.php?email=email
+        //alert (correo);
+        //alert(contrasena);
+        //alert("llamando a ws");
+        $.ajax({
+            url: 'http://192.168.1.9/login.php',
+            data: {email:correo, password:contrasena},
+            type:'post',
+            success: function(response){
+                //alert (response);
+                if(response == correo){
+                    window.location.replace("principal.html");
+                }
+            }
+        });
+        
+       return false;
+    });
+});
---
+++ APPrendoWS/login.php
@@ -0,0 +1,22 @@
+<?php
+echo "hola";
+$email = strval($_POST['email']);
+$password = strval($_POST['password']);
+
+$conn = new mysqli("localhost", "root", "admin", "prueba");
+// Check connection
+if ($conn->connect_error) {
+    die("Connection fallida: " . $conn->connect_error);
+}else
+{
+	$query = "SELECT * FROM usuarios WHERE email='$email' AND password='$password';";
+	$resultado = $conn->query($query);
+	$row = $resultado->fetch_array(MYSQLI_ASSOC);
+	
+	echo $row['email'];
+	
+	
+	$resultado->close();
+}
+$conn->close();
+?>
