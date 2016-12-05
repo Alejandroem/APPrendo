3e465dbb5c8d8e26897f84ab45876e0c8f3c6f0a
Alejandro Enríquez <alexalejandroem@gmail.com>
2016-12-05 14:45:39 -0600

    Desarrollo de juegos DDL 1 y 2

--- APPrendo/www/Juego1.js
+++ APPrendo/www/Juego1.js
@@ -2,12 +2,14 @@
 $(document).ready(function(){
     
     $("#sidenav").load("SideNavLeccion.html");
-    localStorage.setItem("leccion","introdusccion");  
+    localStorage.setItem("leccion","ddl");  
     var leccion = localStorage.getItem("leccion");
     if(leccion=="introduccion"){
         startGame();
-    }else{
+    }else if(leccion=="disenio"){
         startDisenoTablas();
+    }else if(leccion=="ddl"){
+        startDDL();
     }
     localStorage.setItem("usuario","admin");     
     var usuario = localStorage.getItem("usuario");
--- APPrendo/www/Juego2.js
+++ APPrendo/www/Juego2.js
@@ -1,31 +1,34 @@
 $(document).ready(function(){
-    
+
     $("#sidenav").load("SideNavLeccion.html");
-    localStorage.setItem("leccion","intddroduccion");  
+    localStorage.setItem("leccion","ddl");  
     var leccion = localStorage.getItem("leccion");
     if(leccion=="introduccion"){
         startIntroduccionCompletar();
-    }else{
+    }else if(leccion=="disenio"){
         startDisenoRelaciones();
+    }else if(leccion=="ddl"){
+        startDDL(1);
+
     }
-    
+
     localStorage.setItem("usuario","admin");     
     var usuario = localStorage.getItem("usuario");
-       
-  $.ajax({
-            url: 'http://192.168.1.15:8081/perfil.php',
-            data: {usuario:usuario},
-            type:'post',
-            success: function(response){
-                var perfil = JSON.parse(response);
-                //{"idUsuario":"1","nombre":"admin","apellido":"admin","correo":"admin","password":"admin","carnet":"admin","imagen":"admin"}
-               
-                $("#NombreUsuario").text(perfil.carnet);
-                
-                $("#imagenPerfil").attr("src", perfil.imagen);
-                
-            }
-        });
-    
-    
+
+    $.ajax({
+        url: 'http://192.168.1.15:8081/perfil.php',
+        data: {usuario:usuario},
+        type:'post',
+        success: function(response){
+            var perfil = JSON.parse(response);
+            //{"idUsuario":"1","nombre":"admin","apellido":"admin","correo":"admin","password":"admin","carnet":"admin","imagen":"admin"}
+
+            $("#NombreUsuario").text(perfil.carnet);
+
+            $("#imagenPerfil").attr("src", perfil.imagen);
+
+        }
+    });
+
+
 });
