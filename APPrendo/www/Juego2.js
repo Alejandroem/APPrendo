6d582a38555ffb2d0fcad34cf35bdcfa7345d5b1
Alejandro Enríquez <alexalejandroem@gmail.com>
2017-01-07 17:17:23 -0600

    Ultimo commit de juegos revisando

--- APPrendo/www/Juego2.js
+++ APPrendo/www/Juego2.js
@@ -4,27 +4,37 @@ $(document).ready(function(){
     //localStorage.setItem("leccion","ddl");  
     var leccion = localStorage.getItem("leccion");
     if(leccion=="introduccion"){
+        document.getElementById("tituloJuego").textContent = "Introduccion";
         startIntroduccionCompletar();
     }else if(leccion=="disenio"){
+        document.getElementById("tituloJuego").textContent = "Diseño";
         startDisenoRelaciones();
     }else if(leccion=="ddl"){
+        document.getElementById("tituloJuego").textContent = "DDL(Data Definition Language)";
         startDDLSopa();
     }else if(leccion=="dml"){
+        document.getElementById("tituloJuego").textContent = "DML(Data Manipulation Language)";
         /*cambio a consultas startEscribePalabras();*/
         startGame();
     }else if(leccion=="consultas"){
+        document.getElementById("tituloJuego").textContent = "Consultas";
         /*Cambio a consultas complejas startConsultasMemoria();*/
         startEscribePalabras();
     } 
     else if(leccion=="joins"){
+        document.getElementById("tituloJuego").textContent = "Joins";
          startConsultasMemoria();
     }else if(leccion =="subqueries"){
+        document.getElementById("tituloJuego").textContent = "Subqueries";
         subquerysQuerys();
     }else if(leccion =="funciones"){
+        document.getElementById("tituloJuego").textContent = "Funciones";
         startDDLSopa();
     }else if(leccion=="roles"){
+        document.getElementById("tituloJuego").textContent = "Roles y Permisos";
         startEscribePalabras();
     }else if(leccion=="conexion"){
+        document.getElementById("tituloJuego").textContent = "Conexión desde una aplicación externa";
         startConsultasMemoria();
     }
 
